9b58d7b2ab403188f8c6fc7a6a787f5aca137ccc
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2023-07-08 03:59:01 +0300

    game: camera: scroll to zoom

--- src/game/game.hpp
+++ src/game/game.hpp
@@ -5,6 +5,7 @@
 
 struct SDL_KeyboardEvent;
 struct SDL_MouseMotionEvent;
+struct SDL_MouseWheelEvent;
 
 namespace game
 {
@@ -19,6 +20,8 @@ namespace game
     void keydown(SDL_KeyboardEvent&, scene_uid = 0);
     void mouse_motion(SDL_MouseMotionEvent&,
                       scene_uid = 0);
+    void mouse_wheel(SDL_MouseWheelEvent&,
+                    scene_uid = 0);
 }
 
 #endif // INCLUDE_GAME_GAME_HPP
--- src/game/mouse.cpp
+++ src/game/mouse.cpp
@@ -1,6 +1,8 @@
 #include "game/game.hpp"
 #include "game/camera.hpp"
+#include "utils/screen.hpp"
 #include <SDL2/SDL_events.h>
+#include <SDL2/SDL_mouse.h>
 
 
 void game::
@@ -13,5 +15,25 @@ mouse_motion(SDL_MouseMotionEvent& ev, scene_uid)
         camera.mid.x -= ev.xrel / camera.zoom;
         camera.mid.y -= ev.yrel / camera.zoom;
     }
+}
+
+
+void game::
+mouse_wheel(SDL_MouseWheelEvent& ev, scene_uid)
+{
+    auto& camera = game::camera();
+    auto screen = screen_size();
+
+    Point m_pos;
+    SDL_GetMouseState(&m_pos.x, &m_pos.y);
+
+    auto old_zoom = camera.zoom;
+    camera.zoom += ev.y * 0.15f;
+
+    if(camera.zoom < 0.5)
+        camera.zoom = 0.5;
+
+    if(camera.zoom > 2.5)
+        camera.zoom = 2.5;
 }
 
